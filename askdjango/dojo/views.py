3eec1e59aa5ca49d6bc9b57716b8c373a12ff33c
류다윤 <ekdbs3675@naver.com>
2017-09-05 16:55:50 +0900

    URLconf and Regular Expression 수업끝

--- askdjango/dojo/views.py
+++ askdjango/dojo/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def mysum(request, x, y=0 ,z=0):
-     return HttpResponse(int(x)+int(y)+int(z) )
+def mysum(request, numbers):
+    result = sum(map(lambda numbers: int(numbers or 0), numbers.split("/")))
+    return HttpResponse(result)
+
+def hello(requset, name, age):
+    return HttpResponse('안녕하세요. {}. {}살이시네요.'.format(name,age))
