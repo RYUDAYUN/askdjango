da7d76b457811f3fd65809f92ce6d3e2a21b51d1
류다윤 <ekdbs3675@naver.com>
2017-09-08 23:43:59 +0900

    validator

---
+++ C++/HelloCpp/HelloCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloCpp
+//
+//  Created by 류다윤 on 2017. 9. 8..
+//  Copyright © 2017년 류다윤. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
